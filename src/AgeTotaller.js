92c3acd2f0c14fa8682f2ccecfad91b3c7286780
Bahia369 <hsbcoding@gmail.com>
2021-06-03 19:22:02 +0100

    age totaller
    updated

--- src/AgeTotaller.js
+++ src/AgeTotaller.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import AgeCounter from "./AgeCounter";
 
 const AgeTotaller = () => {
+  const [cats, setCats] = useState(["Babbu", "Pappu", "Laddu", "Bappu"]);
+  const [newCat, setNewcat] = useState("");
+
   return (
     <div className="AgeTotaller">
       <div className="cats"></div>
