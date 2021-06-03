e262119e5c9b1555b27a58205a28d969d6077094
Bahia369 <hsbcoding@gmail.com>
2021-06-03 18:13:10 +0100

    buttons
    buttons code has been finished

--- src/App.js
+++ src/App.js
@@ -9,15 +9,3 @@ export default function App() {
     </div>
   );
 }
-const AgeCounter = () => {
-  //  2 Buttons with on click.
-
-  console.log(AgeCounter);
-  <div classname=".AgeCounter">
-    <button>Add A Year</button>
-    <button>Add New Life</button>
-    <h3>Display Current Age</h3>
-  </div>;
-};
-
-// 1. Create a AgeCounter component which uses useState to store the current age and has two buttons - one to add a year (increment state by 1) and one to add a new life (set state back to 0). Create these functions and use the onClick event on the buttons to use them. Display the current age within the AgeCounter component in a h3 tag. Use a className of ".AgeCounter" for the component (it should be a div with a h3 tag and two buttons inside, the CSS is already provided in styles.css).
