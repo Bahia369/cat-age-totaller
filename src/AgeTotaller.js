70eb2b0734cb54a158997487879a6eef886942b7
Bahia369 <hsbcoding@gmail.com>
2021-06-03 16:53:52 +0100

    updated
    buttons added

--- src/AgeTotaller.js
+++ src/AgeTotaller.js
@@ -8,4 +8,19 @@ const AgeTotaller = () => {
   );
 };
 
+
+
+
 export default AgeTotaller;
+
+
+const AgeCounter = () => {
+
+return
+<button onclick ={}>Add A Year</button> 
+<button onclick ={}>Add New Life</button>
+}
+
+
+
+// 1. Create a AgeCounter component which uses useState to store the current age and has two buttons - one to add a year (increment state by 1) and one to add a new life (set state back to 0). Create these functions and use the onClick event on the buttons to use them. Display the current age within the AgeCounter component in a h3 tag. Use a className of ".AgeCounter" for the component (it should be a div with a h3 tag and two buttons inside, the CSS is already provided in styles.css).
