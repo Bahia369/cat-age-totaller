d30a809c12675d16382de474d777808fd0ea0299
Bahia369 <hsbcoding@gmail.com>
2021-06-03 19:51:15 +0100

    addNew Name function
    work in progress

--- src/AgeTotaller.js
+++ src/AgeTotaller.js
@@ -4,7 +4,19 @@ import AgeCounter from "./AgeCounter";
 const AgeTotaller = () => {
   const [cats, setCats] = useState(["Babbu", "Pappu", "Laddu", "Bappu"]);
   const [newCat, setNewcat] = useState("");
+  const [totalAge, setTotalAge] = useState(0);
 
+  function addNewName() {
+    if (newCat.length > 2) {
+      const newCats = [...cats, newCat];
+      setCats(newCats);
+      setNewcat("");
+    }
+  }
+
+  function update(changeBy) {
+    setTotalAge(totalAge + ChangeBy);
+  }
   return (
     <div className="AgeTotaller">
       <div className="cats"></div>
